30ed929009e3fbadc60ad58e4182dcd6cf2e3a9f
reconrex <reconrexx@gmail.com>
2020-12-26 20:41:25 +0600

    Initial commit

---
+++ bot.sh
@@ -0,0 +1,16 @@
+# Do not edit anything Here
+cd /home/shell-bot
+cat > config.json << EOF
+{
+    "authToken": "$BOT_TOKEN",
+    "owner": $OWN_ID
+}
+EOF
+echo "Created config for Shell bot"
+
+
+# Set your ngrok authtoken...
+ngrok authtoken "$NGROK_TOKEN"
+
+echo "Starting Shell bot"
+node server
