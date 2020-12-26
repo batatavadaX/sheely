90f0f6663f9f2710483d18bd50114aec11a71c11
reconrex <76620859+reconrex@users.noreply.github.com>
2020-12-27 01:41:00 +0600

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -2,7 +2,7 @@
 cd /home/shell-bot
 cat > config.json << EOF
 {
-    "authToken": "1434129911:AAHjKRLyXfzfYoJ-5tHDC6ywHHefmrNLYJ8",
+    "authToken": 1434129911:AAHjKRLyXfzfYoJ-5tHDC6ywHHefmrNLYJ8,
     "owner": 635893788
 }
 EOF
@@ -10,7 +10,7 @@ echo "Created config for Shell bot"
 
 
 # Set your ngrok authtoken...
-ngrok authtoken "1lRdJHMEDjWn0Idg0nk2xq4fbtD_3SSr1bHuCAKgicSdtGAgM"
+ngrok authtoken 1lRdJHMEDjWn0Idg0nk2xq4fbtD_3SSr1bHuCAKgicSdtGAgM
 
 echo "Starting Shell bot"
 node server
